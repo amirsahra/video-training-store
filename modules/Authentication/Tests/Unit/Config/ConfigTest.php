2efb390babd3503326c5a5086d3e5b87132c5bd3
amirsahra <amirhosein.sahra@gmail.com>
2023-04-30 14:58:28 +0330

    feat(Testing): config test

---
+++ modules/Authentication/Tests/Unit/Config/ConfigTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Authentication\Tests\Unit\Config;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertTrue(true);
+    }
+}
